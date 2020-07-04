5a34aab641190f318f467246eccf3c7c9a43a7f0
soul-reaper46 <sujaysn46@gmail.com>
2020-07-04 14:10:32 +0530

    Update index file

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ import {name as appName} from './app.json';
 import adm_home from './adm_home';
 import navigation from './navigation';
 import fb from './fb';
+import create_usr from './create_usr';
+import all_usr from './all_usr';
+import Log_out from './Log_out';
+import mod_usr from './mod_usr';
+import user_home from './user_home';
 
 AppRegistry.registerComponent(appName, () => navigation);
