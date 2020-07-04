5079d5bd4e726b2c578ba56790a8b723a74f5042
soul-reaper46 <sujaysn46@gmail.com>
2020-07-04 14:10:02 +0530

    add empty usr_home

---
+++ user_home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class App extends React.Component {
+   render() {
+     return (
+       <View>
+         <Text>Hello, world!</Text>
+       </View>
+      );
+    }
+  }
+
+
+
+
+
